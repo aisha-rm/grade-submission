a973d0c17622cdf976d8a32c4078d967a5b7c110
Aisha Mohammed <80531561+aisha-rm@users.noreply.github.com>
2023-02-04 02:00:43 +0000

    implemented grade repo, service and controller

--- src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
+++ src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
@@ -1,13 +1,38 @@
 package com.ltp.gradesubmission;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.repository.StudentRepository;
+
 @SpringBootApplication
-public class GradeSubmissionApplication {
+public class GradeSubmissionApplication implements CommandLineRunner  {
+
+	@Autowired
+	StudentRepository studentRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradeSubmissionApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//this would run everytime the app is run or restarted to repopulate the db 
+		Student[] students = new Student[] {
+            new Student(1L, "Musty Trouble", LocalDate.parse(("1980-07-31"))),
+            new Student(2L, "Aisha Peace", LocalDate.parse(("1980-03-01"))),
+            new Student(3L, "Hadiza Problem", LocalDate.parse(("1979-09-19"))),
+            new Student(4L, "Amina Issues", LocalDate.parse(("1980-07-30"))) 
+        };
+		
+		for(int i = 0; i < students.length; i++){
+			studentRepository.save(students[i]);
+		}
 	}
 
 }
--- src/main/java/com/ltp/gradesubmission/web/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/web/GradeController.java
@@ -1,5 +1,6 @@
 package com.ltp.gradesubmission.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,12 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+import com.ltp.gradesubmission.service.GradeService;
 import com.ltp.gradesubmission.entity.Grade;
 
 
 @RestController
 @RequestMapping("/grade")
 public class GradeController {
+
+    @Autowired
+    GradeService gradeService;
     
     @GetMapping("/student/{studentId}/course/{courseId}")
     public ResponseEntity<Grade> getGrade(@PathVariable Long studentId, @PathVariable Long courseId) {
@@ -31,7 +36,7 @@ public class GradeController {
 
     @PutMapping("/student/{studentId}/course/{courseId}")
     public ResponseEntity<Grade> updateGrade(@RequestBody Grade grade, @PathVariable Long studentId, @PathVariable Long courseId){
-        return new ResponseEntity<>(grade, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
     
     @DeleteMapping("/student/{studentId}/course/{courseId}")
